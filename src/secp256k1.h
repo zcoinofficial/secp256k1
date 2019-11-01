f8907272ff78ddef8ae5c3a5b4e6215dfeafbe54
Putta Khunchalee <putta@zcoin.io>
2019-11-01 17:32:57 +0700

    Migrate C++ wrappers

---
+++ src/secp256k1.h
@@ -0,0 +1,15 @@
+#ifndef SECP256K1_SECP256K1_H
+#define SECP256K1_SECP256K1_H
+
+#include "ecmult.h"
+#include "ecmult_gen.h"
+#include "util.h"
+
+struct secp256k1_context_struct {
+    secp256k1_ecmult_context ecmult_ctx;
+    secp256k1_ecmult_gen_context ecmult_gen_ctx;
+    secp256k1_callback illegal_callback;
+    secp256k1_callback error_callback;
+};
+
+#endif /* SECP256K1_SECP256K1_H */
